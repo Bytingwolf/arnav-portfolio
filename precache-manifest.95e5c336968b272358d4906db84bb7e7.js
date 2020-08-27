820d038c8633cbee0ed0efad11e48915925dd62c
Bytingwolf <arnav1997@gmail.com>
2020-08-27 20:57:13 +0100

    Updates

---
+++ precache-manifest.95e5c336968b272358d4906db84bb7e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4131de50b36cecbd8582701d25afeaef",
+    "url": "/arnav-portfolio/index.html"
+  },
+  {
+    "revision": "ca2a98b9c095fadba8d7",
+    "url": "/arnav-portfolio/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "3ae4d2d865398bf6de4d",
+    "url": "/arnav-portfolio/static/js/2.48c9b7c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arnav-portfolio/static/js/2.48c9b7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2a98b9c095fadba8d7",
+    "url": "/arnav-portfolio/static/js/main.b347ae75.chunk.js"
+  },
+  {
+    "revision": "15ea18ded3b5fa2bb8ac",
+    "url": "/arnav-portfolio/static/js/runtime-main.0fe176a9.js"
+  }
+]);
